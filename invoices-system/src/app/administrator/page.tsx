5757841ef1927a474b1d9d7a568ed448cd968578
Kacper <kacperlewicki.js@gmail.com>
2024-11-24 20:15:15 +0100

    add new page - administrator

---
+++ invoices-system/src/app/administrator/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from 'react';
+import AdminPage from '../../pages/admin/adminPage';
+
+export default function administratorPage ()  {
+ 
+  return (
+    
+    <div>
+
+        <AdminPage />
+   
+    </div>
+
+  );
+};
+
