c50eae27cced097e26e761632fa30b52a278d5aa
Kacper <kacperlewicki.js@gmail.com>
2025-01-25 13:00:18 +0100

    database connection

--- invoices-system/src/pages/api/lib/db.js
+++ invoices-system/src/pages/api/lib/db.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 const connection = mysql.createPool({
   host: 'sql7.freesqldatabase.com',
-  user: 'sql7758332',
-  password: 'AJfJG9b5Kn',
-  database: 'sql7758332',
+  user: 'sql7759493',
+  password: 'qDeSZsz8R3',
+  database: 'sql7759493',
 });
 
 export default connection;
